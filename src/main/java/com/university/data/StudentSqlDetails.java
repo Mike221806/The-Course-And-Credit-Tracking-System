49f1607415329ad5bef2da405800ee88392e5286
Mahyawit <Mahialem48@gmail.com>
2026-02-01 11:36:24 +0300

    added essential imports to StudentSqlDetails.java

--- src/main/java/com/university/data/StudentSqlDetails.java
+++ src/main/java/com/university/data/StudentSqlDetails.java
@@ -1,4 +1,14 @@
 package com.university.data;
 
-public class StudentSqlDetails {
-}
+import com.university.roles.Student;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * SQLite implementation of StudentDetails interface.
+ * Demonstrates concrete implementation of persistence layer following the DIP principle.
+ * Provides CRUD operations for student data using SQLite database.
+ */
