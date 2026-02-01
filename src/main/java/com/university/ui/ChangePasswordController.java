8977db4343e787f5161d2a1594035d3f787a94f2
marrygech <merongetnetb7@gmail.com>
2026-02-01 18:13:23 +0300

    addition of essential imports in ChangePasswordController.java

---
+++ src/main/java/com/university/ui/ChangePasswordController.java
@@ -0,0 +1,14 @@
+package com.university.ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import com.university.roles.Student;
+import com.university.data.StudentDetails;
+import com.university.data.StudentSqlDetails;
+import com.university.services.PasswordUtil;
+
+/**
+ * Controller for the Change Password dialog.
+ * Allows students to change their own password.
+ */
