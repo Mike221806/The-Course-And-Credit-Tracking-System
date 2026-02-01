a3d6c2939f255b239f0eae666310a8694e27150c
mihretugr-6680-17-star <mihret.ugr-6880-17@aau.edu.et>
2026-02-01 16:33:25 +0300

    vital imports for CourseService.java

---
+++ src/main/java/com/university/services/CourseService.java
@@ -0,0 +1,15 @@
+package com.university.services;
+
+import com.university.data.CourseDetails;
+import com.university.data.EnrollmentDetails;
+import com.university.courses.Course;
+import com.university.courses.Enrollment;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Service for managing courses and enrollments.
+ * Demonstrates Single Responsibility Principle (SRP) by focusing on course and enrollment operations.
+ * Follows Dependency Inversion Principle (DIP) by depending on interfaces.
+ */
