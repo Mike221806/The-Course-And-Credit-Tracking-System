4a10e3c92818afe28f74603f1bdd67a6316612d1
mihretugr-6680-17-star <mihret.ugr-6880-17@aau.edu.et>
2026-02-01 16:33:00 +0300

    vital imports for Exam.java

---
+++ src/main/java/com/university/courses/Exam.java
@@ -0,0 +1,9 @@
+package com.university.courses;
+
+import java.time.LocalDate;
+
+/**
+ * Abstract base class for exam types.
+ * Extends CourseworkItem and provides common functionality for all exam types.
+ * Demonstrates abstraction and supports the inheritance hierarchy.
+ */
