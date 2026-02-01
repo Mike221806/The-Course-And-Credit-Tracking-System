d09fc2897ff69651f5a62d92cf3f28fcb129d8d8
marrygech <merongetnetb7@gmail.com>
2026-02-01 18:10:53 +0300

    addition of essential imports in AdvisorSearchStudentController.java

---
+++ src/main/java/com/university/ui/AdvisorSearchStudentController.java
@@ -0,0 +1,11 @@
+package com.university.ui;
+
+import com.university.roles.Advisor;
+import com.university.data.StudentDetails;
+import com.university.data.StudentSqlDetails;
+import com.university.roles.Student;
+
+/**
+ * Controller for searching students.
+ * Allows advisors to find and select students.
+ */
