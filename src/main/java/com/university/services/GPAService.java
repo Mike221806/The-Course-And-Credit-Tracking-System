54f115ab2fdaa5a0730b0272dbe276db1c1cb693
mihretugr-6680-17-star <mihret.ugr-6880-17@aau.edu.et>
2026-02-01 16:34:36 +0300

    vital imports for GPAService.java

---
+++ src/main/java/com/university/services/GPAService.java
@@ -0,0 +1,16 @@
+package com.university.services;
+
+import com.university.data.EnrollmentDetails;
+import com.university.data.StudentDetails;
+import com.university.data.CourseDetails;
+import com.university.courses.Enrollment;
+import com.university.courses.Course;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Service for calculating and managing GPA and academic progress.
+ * Demonstrates Single Responsibility Principle (SRP) by focusing on GPA calculations.
+ * Follows Dependency Inversion Principle (DIP) by depending on interfaces.
+ */
