72959b4d2096914032ba2ed897753bdd6728bf43
Mahyawit <Mahialem48@gmail.com>
2026-02-01 11:32:11 +0300

    added essential imports to CourseworkSqlDetails.java

--- src/main/java/com/university/data/CourseworkSqlDetails.java
+++ src/main/java/com/university/data/CourseworkSqlDetails.java
@@ -1,4 +1,17 @@
 package com.university.data;
 
-public class CourseworkSqlDetails {
-}
+import com.university.courses.*;
+import com.university.courses.Assignment;
+import com.university.courses.FinalExam;
+import com.university.courses.MidtermExam;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * SQLite implementation of CourseworkDetails interface.
+ * Demonstrates concrete implementation of persistence layer following the DIP principle.
+ * Provides CRUD operations for coursework data using SQLite database.
+ */
