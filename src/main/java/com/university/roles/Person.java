92aee1800e908fa826a8cf4515d2878a6cd4b5ac
Mike221806 <mikeymekuira@gmail.com>
2026-02-01 15:34:30 +0300

    added important commnents to explain Person.java

---
+++ src/main/java/com/university/roles/Person.java
@@ -0,0 +1,6 @@
+package com.university.roles;
+
+/**
+ * Abstract base class representing a person in the university system.
+ * Demonstrates encapsulation with private fields and provides abstraction for Student and Advisor classes.
+ */
