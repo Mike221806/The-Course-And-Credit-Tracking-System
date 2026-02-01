9389f363b99af0d1a9261d1673290548c8f3a437
Mahyawit <Mahialem48@gmail.com>
2026-02-01 11:29:51 +0300

    added essential imports to CourseSqlDetails.java

--- src/main/java/com/university/data/CourseSqlDetails.java
+++ src/main/java/com/university/data/CourseSqlDetails.java
@@ -1,4 +1,14 @@
 package com.university.data;
 
-public class CourseSqlDetails {
-}
+import com.university.courses.Course;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * SQLite implementation of CourseDetails interface.
+ * Demonstrates concrete implementation of persistence layer following the DIP principle.
+ * Provides CRUD operations for course data using SQLite database.
+ */
