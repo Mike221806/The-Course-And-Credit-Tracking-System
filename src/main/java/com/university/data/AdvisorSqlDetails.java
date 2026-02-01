f364a908b02c76bdeeef0a26cb0ed9267537e123
Mahyawit <Mahialem48@gmail.com>
2026-02-01 11:27:50 +0300

    added essential imports to AdvisorSqlDetails.java

--- src/main/java/com/university/data/AdvisorSqlDetails.java
+++ src/main/java/com/university/data/AdvisorSqlDetails.java
@@ -1,4 +1,14 @@
 package com.university.data;
 
-public class AdvisorSqlDetails {
-}
+import com.university.roles.Advisor;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * SQLite implementation of AdvisorDetails interface.
+ * Demonstrates concrete implementation of persistence layer following the DIP principle.
+ * Provides CRUD operations for advisor data using SQLite database.
+ */
