b60f30b038a77387d150e35be6ea7c0c48811024
Mike221806 <mikeymekuira@gmail.com>
2026-02-01 15:32:28 +0300

    added important imports for App.java

---
+++ src/main/java/com/university/App.java
@@ -0,0 +1,13 @@
+package com.university;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import com.university.data.DatabaseInitializer;
+
+/**
+ * Main application class for the Course & Credit Tracking System.
+ * Initializes the database and launches the JavaFX application.
+ */
