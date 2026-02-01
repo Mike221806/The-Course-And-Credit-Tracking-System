3c71d08b58387e3dc63d869cab12efe242bba0b6
marrygech <merongetnetb7@gmail.com>
2026-02-01 18:14:23 +0300

    addition of essential imports in StudentDashboardController.java

---
+++ src/main/java/com/university/ui/StudentDashboardController.java
@@ -0,0 +1,19 @@
+package com.university.ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+import com.university.roles.Student;
+
+import java.io.IOException;
+
+/**
+ * Controller for the Student Dashboard.
+ * Provides navigation to different student functions and displays student information.
+ */
