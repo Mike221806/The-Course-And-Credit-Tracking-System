36b68bbdf0692bee4a9fd9daac034f17dab52150
Mike221806 <mikeymekuira@gmail.com>
2026-02-01 15:36:41 +0300

    added important imports for CircularGPAIndicator.java

---
+++ src/main/java/com/university/ui/components/CircularGPAIndicator.java
@@ -0,0 +1,10 @@
+package com.university.ui.components;
+
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+
+/**
+ * Custom circular GPA indicator that displays as a ring/bar around the GPA number.
+ * The ring fills up and changes color based on GPA value.
+ */
