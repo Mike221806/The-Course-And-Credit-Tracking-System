8a7f983b12a8002f5dc60f1eb536e7b643be2c0c
Mike221806 <mikeymekuira@gmail.com>
2026-02-01 15:34:59 +0300

    added important imports for AuthService.java

---
+++ src/main/java/com/university/services/AuthService.java
@@ -0,0 +1,15 @@
+package com.university.services;
+
+import com.university.data.StudentDetails;
+import com.university.data.AdvisorDetails;
+import com.university.roles.Student;
+import com.university.roles.Advisor;
+
+
+import java.util.Optional;
+
+/**
+ * Authentication service for user login and validation.
+ * Demonstrates Single Responsibility Principle (SRP) by focusing only on authentication.
+ * Follows Dependency Inversion Principle (DIP) by depending on interfaces.
+ */
