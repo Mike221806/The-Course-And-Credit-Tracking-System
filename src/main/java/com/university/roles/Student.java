726de0b31cf4bcecc2a7580ab31fb40aa4b12eca
marrygech <merongetnetb7@gmail.com>
2026-02-01 18:02:48 +0300

    addition of student role in Student.java

---
+++ src/main/java/com/university/roles/Student.java
@@ -0,0 +1,7 @@
+package com.university.roles;
+
+/**
+ * Represents a student in the university system.
+ * Inherits from Person and adds student-specific attributes.
+ * Demonstrates inheritance and can be used polymorphically as a Person.
+ */
