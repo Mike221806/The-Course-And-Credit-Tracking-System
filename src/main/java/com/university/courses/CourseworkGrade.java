9a8572c0eb6b4a4dc299d45f4e5c531e29840dd4
mihretugr-6680-17-star <mihret.ugr-6880-17@aau.edu.et>
2026-02-01 16:31:13 +0300

    vital imports for CourseworkGrade.java

---
+++ src/main/java/com/university/courses/CourseworkGrade.java
@@ -0,0 +1,6 @@
+package com.university.courses;
+
+/**
+ * Represents a grade for a specific coursework item for a student.
+ * Demonstrates encapsulation and links students to their coursework performance.
+ */
