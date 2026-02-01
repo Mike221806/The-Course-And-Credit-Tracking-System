468a9d12ce0563d0af3b368870b141fbb5b4b37d
marrygech <merongetnetb7@gmail.com>
2026-02-01 18:02:15 +0300

    addition of advisor role in Advisor.java

---
+++ src/main/java/com/university/roles/Advisor.java
@@ -0,0 +1,7 @@
+package com.university.roles;
+
+/**
+ * Represents an academic advisor in the university system.
+ * Inherits from Person and adds advisor-specific attributes.
+ * Demonstrates inheritance and can be used polymorphically as a Person.
+ */
