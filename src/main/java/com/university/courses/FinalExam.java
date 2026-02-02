8b1939fc71d1e5ebc37486cf9f1cff797ea58997
naomieshetayehu-code <naomieshetayehu19@gmail.com>
2026-02-02 05:30:58 +0300

    adding important imports to FinalExam.java

---
+++ src/main/java/com/university/courses/FinalExam.java
@@ -0,0 +1,9 @@
+package com.university.courses;
+
+import java.time.LocalDate;
+
+/**
+ * Represents a final exam.
+ * Extends Exam and provides specific implementation for final exam type.
+ * Demonstrates inheritance and polymorphism in the coursework hierarchy.
+ */
